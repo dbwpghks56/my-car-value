5341ae2d4457fb09ddf31cbd50566cd76efd48a8
wpghks <wpghks56@naver.com>
2023-11-14 17:28:22 +0900

    [feature] create current-user interceptor

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -2,9 +2,14 @@ import {
     ExecutionContext,
     createParamDecorator
 } from '@nestjs/common'
+import { User } from '../domain/user.entity';
+
 
 export const CurrentUser = createParamDecorator(
-    (data: any, context: ExecutionContext) => {
-        return 'hi there!';
+    (data: never, context: ExecutionContext):User => {
+        const request = context.switchToHttp().getRequest();
+        console.log(request.session.userId);
+
+        return request.currentUser;
     }
 )
---
+++ src/users/interceptors/current-user.interceptor.ts
@@ -0,0 +1,24 @@
+import { NestInterceptor, ExecutionContext, CallHandler, Injectable } from "@nestjs/common";
+import { UsersService } from "../users.service";
+import { Observable } from "rxjs";
+import { User } from "../domain/user.entity";
+
+@Injectable()
+export class CurrentUserInterceptor implements NestInterceptor {
+    constructor(
+        private readonly userService:UsersService
+    ){}
+
+    async intercept(context: ExecutionContext, next: CallHandler<any>) {
+        const request = context.switchToHttp().getRequest();
+        const {userId} = request.session || {};
+
+        if(userId) {
+            const user:User = await this.userService.findById(userId);
+            request.currnetUser = user;
+        }
+
+        return next.handle();
+    }
+
+}
