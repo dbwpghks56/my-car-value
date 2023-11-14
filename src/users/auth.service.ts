7bf64bf27911218a1a9842468114064e0350bc9a
wpghks <wpghks56@naver.com>
2023-11-14 14:19:09 +0900

    [feature] password encrypt

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,10 @@
 import {BadRequestException, Injectable} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './domain/user.entity';
-import { randomBytes, scrypt } from 'crypto';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -21,14 +24,16 @@ export class AuthService {
 
         // Hash the users password 
         // Generate a salt
+        const salt = randomBytes(8).toString('hex');
 
         // hash the salt and the password together
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
 
         // join the hashed result and the salt together
-
-
+        const result = salt + '.' + hash.toString('hex');
 
         // Create a new user and save it
+        
 
         // return the user
 
