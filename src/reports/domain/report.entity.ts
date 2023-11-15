603d11de9396a3e5f2286c842b8c2a47c6563334
wpghks <wpghks56@naver.com>
2023-11-15 11:07:48 +0900

    [feature] change report entity columns

--- src/reports/domain/report.entity.ts
+++ src/reports/domain/report.entity.ts
@@ -10,4 +10,22 @@ export class Report extends BaseEntity {
 
     @Column()
     price: number;
+
+    @Column()
+    make: string;
+
+    @Column()
+    model: string;
+
+    @Column()
+    year: number;
+
+    @Column()
+    lng: number;
+
+    @Column()
+    lat: number;
+
+    @Column()
+    mileage: number;
 }
