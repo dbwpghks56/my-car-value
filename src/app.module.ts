9abb7515f00810613a7771c9df88fb21c00aecd4
wpghks <wpghks56@naver.com>
2023-11-15 10:07:49 +0900

    [feature] 환경 나누기

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -8,19 +9,43 @@ import { User } from './users/domain/user.entity';
 import { Report } from './reports/domain/report.entity';
 
 @Module({
-  imports: [UsersModule, ReportsModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'svc.sel5.cloudtype.app',
-    port: 32119,
-    username: 'root',
-    password: 'mydb123',
-    database: 'postgres',
-    entities: [User, Report],
-    // 데이터베이스의 테이블을 살펴본 뒤 구조를 자동으로 업데이트한다.
-    // jpa 로 치면 ddl: update 너낌
-    synchronize: true,
-    logging: true
-  })],
+  imports: [UsersModule, ReportsModule, 
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'svc.sel5.cloudtype.app',
+    //   port: 32119,
+    //   username: 'root',
+    //   password: 'mydb123',
+    //   database: 'postgres',
+    //   entities: [User, Report],
+    //   // 데이터베이스의 테이블을 살펴본 뒤 구조를 자동으로 업데이트한다.
+    //   // jpa 로 치면 ddl: update 너낌
+    //   synchronize: true,
+    //   logging: true
+    // }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'),
+          port: 32119,
+          username: 'root',
+          password: 'mydb123',
+          database: 'postgres',
+          entities: [User, Report],
+          // 데이터베이스의 테이블을 살펴본 뒤 구조를 자동으로 업데이트한다.
+          // jpa 로 치면 ddl: update 너낌
+          synchronize: true,
+          logging: true
+        }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
